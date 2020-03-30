ca2ac784437d6f62043061a5b29ab1c3bed25fb3
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-03-30 18:02:28 +0100

    interfaces solution in python3

---
+++ day19_interfaces.py
@@ -0,0 +1,19 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    
+    def divisorSum(self, n):
+        total = 0;
+        for num in range(1, n + 1):
+            if(n % num == 0):
+                total += num
+        return total
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
